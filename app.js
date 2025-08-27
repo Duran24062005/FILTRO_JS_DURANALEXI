55d5617c917b754f8a9fdf2946af7f62bb7e0466
Alexi_Duran <Alexisoft01@gmail>
2025-08-27 12:32:44 -0500

    feat: :fire: finish first stage to script to project

--- app.js
+++ app.js
@@ -17,13 +17,38 @@ document.addEventListener('DOMContentLoaded', async () => {
     
     
     function renderMovies(movies) {
+        const container = document.getElementById('main-container');
+        console.log(movies.description);
+        container.innerHTML = "";
+        movies.description.forEach(m => {
+            container.innerHTML += `<div class="card-container">
+            <div class="head-img">
+                <div class="img-container">
+                    <img src="${m['#IMG_POSTER']}" alt="Image to${m['#TITLE']}" class="img">
+                </div>
+                <div class="info-container">
+                    <h1>${m['#TITLE']}</h1>
+                    <h2>Actors: ${m['#ACTORS']}</h2>
+                    <p><strong>Fecha de creación:</strong> ${m['#YEAR']}</p>
+                </div>
+            </div>
+        </div>
+            `;
+        });
     }
     
     
     btnSearch.addEventListener('click', async() => {
-        let ww = await fetchMovies('vida');
-        // console.log(ww.description);
         // console.log(inputData.value);
+        let data = inputData.value.toLowerCase().trim();
+        /// console.log(data);
+        if (data !== '') {
+            let ww = await fetchMovies(data);
+            //console.log(ww.description);
+            renderMovies(ww);
+        }
 
     })
 });
+
+
